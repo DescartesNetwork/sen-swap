7db13f6f01d3bdfef21428d848f02ac5f73a8cea
tuphan-dn <tuphan@descartes.network>
2021-12-10 14:48:25 +0700

    remove advanced mode

--- src/app/widget/index.tsx
+++ src/app/widget/index.tsx
@@ -1,11 +1,6 @@
-import { useCallback } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-
-import { Row, Col, Typography, Space, Button } from 'antd'
+import { Row, Col, Typography, Space } from 'antd'
 import IonIcon from 'shared/antd/ionicon'
 
-import { AppDispatch, AppState } from 'app/model'
-import { increaseCounter } from 'app/model/main.controller'
 import { env } from 'shared/runtime'
 import { useUI } from 'senhub/providers'
 
@@ -13,9 +8,6 @@ const Widget = () => {
   const {
     ui: { width, infix },
   } = useUI()
-  const dispatch = useDispatch<AppDispatch>()
-  const { counter } = useSelector((state: AppState) => state.main)
-  const increase = useCallback(() => dispatch(increaseCounter()), [dispatch])
 
   return (
     <Row gutter={[24, 24]}>
@@ -29,12 +21,6 @@ const Widget = () => {
         <Typography.Text>
           Env: {env} - {width}px - {infix}
         </Typography.Text>
-      </Col>
-      <Col>
-        <Typography.Text>Counter: {counter}</Typography.Text>
-      </Col>
-      <Col>
-        <Button onClick={increase}>Increase</Button>
       </Col>
     </Row>
   )
