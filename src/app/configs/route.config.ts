f3fcba25c408a7bd96eb969c98c563030fc09dc3
tuanle161099 <tuanle@descartes.network>
2022-01-12 14:57:59 +0700

    feature/ wormhole support

---
+++ src/app/configs/route.config.ts
@@ -0,0 +1,33 @@
+import { Net } from 'shared/runtime'
+
+/**
+ * Contructor
+ */
+type Config = {
+  assetsRoute: string
+}
+
+const config: Record<Net, Config> = {
+  /**
+   * Development configurations
+   */
+  devnet: {
+    assetsRoute: '/app/sen_assets',
+  },
+
+  /**
+   * Staging configurations
+   */
+  testnet: {
+    assetsRoute: '/app/sen_assets',
+  },
+
+  /**
+   * Production configurations
+   */
+  mainnet: {
+    assetsRoute: '/app/sen_assets',
+  },
+}
+
+export default config
