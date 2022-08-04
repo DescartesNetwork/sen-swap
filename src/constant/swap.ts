73cd9430f18374701cee21cf9f3d326a3c8ae4a6
The Vinh <vinhnguyen@descartes.network>
2022-08-04 14:13:31 +0700

    add segmented

--- src/constant/swap.ts
+++ src/constant/swap.ts
@@ -2,3 +2,8 @@ export enum PriceImpact {
   goodSwap = 0.01, // price impact lower than 1%
   acceptableSwap = 0.05, // price impact lower than 5%
 }
+
+export const HOMEPAGE_TABS: Record<string, string> = {
+  Swap: 'swap',
+  Pools: 'pools',
+}
